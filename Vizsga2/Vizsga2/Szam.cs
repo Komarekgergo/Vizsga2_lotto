9aba8ad82b692053ed3eb056a642db21b49769ef
Komarekgergo <71069881+Komarekgergo@users.noreply.github.com>
2022-03-24 11:00:12 +0100

    Add files via upload

---
+++ Vizsga2/Vizsga2/Szam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vizsga2
+{
+    class Szam
+    {
+        public int szam;
+        public int db;
+
+        public Szam(int szam, int db)
+        {
+            this.szam = szam;
+            this.db = db;
+        }
+    }
+}
